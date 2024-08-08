b14c841400cf4b3b71867aa5a90e939b2bb68346
richie-south <richard.soderman@gmail.com>
2024-08-08 12:54:51 +0200

    game

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,58 +1,184 @@
-import {useEffect, useReducer} from 'react'
-import Button from 'react-bootstrap/esm/Button'
-import Card from 'react-bootstrap/esm/Card'
-import Container from 'react-bootstrap/esm/Container'
-import Nav from 'react-bootstrap/esm/Nav'
-import Navbar from 'react-bootstrap/esm/Navbar'
-import {Route, Switch, useHistory, useLocation} from 'react-router'
-import {Link} from 'react-router-dom'
+import { useEffect, useReducer } from "react"
+import Button from "react-bootstrap/esm/Button"
+import Card from "react-bootstrap/esm/Card"
+import Container from "react-bootstrap/esm/Container"
+import Nav from "react-bootstrap/esm/Nav"
+import Navbar from "react-bootstrap/esm/Navbar"
+import { Route, Switch, useHistory, useLocation } from "react-router"
+import { Link } from "react-router-dom"
 import {
   getFoundQrCodes,
   isFirstFind,
   QRCodes,
-  qrCode0,
   qrCode1,
   qrCode2,
   qrCode3,
   qrCode4,
+  qrCode5,
+  qrCode6,
+  qrCode7,
+  qrCode8,
+  qrCode9,
+  qrCode10,
+  qrCode11,
+  qrCode12,
+  qrCode13,
+  qrCode14,
+  qrCode15,
+  qrCode16,
+  qrCode17,
+  qrCode18,
   setFirstFind,
+  saveFoundQRCode,
   resetGame,
-  saveFoundQRCode
-} from './lib/localstorage-handler'
-import {useQuery} from './lib/use-query'
-import qrImage0 from './qr-677d572f-1392-475a-bd70-491b530f6dfd.png'
-import qrImage1 from './qr-a47a540d-3713-4270-8b00-a5101ee7be07.png'
-import qrImage2 from './qr-cfccb9ab-adab-45f3-9b8c-652815e84fdb.png'
-import qrImage3 from './qr-f1f97ade-f59a-4034-9d28-575d587a124d.png'
-import qrImage4 from './qr-62937f4e-e76d-4a99-8372-175efc548194.png'
-import Alert from 'react-bootstrap/esm/Alert'
- 
+} from "./lib/localstorage-handler"
+import { useQuery } from "./lib/use-query"
+import Alert from "react-bootstrap/esm/Alert"
+
+import qr01 from "./01-8ce5e78f-71c6-4827-9f52-0a4762c0e62c.png"
+import qr02 from "./02-c6c08b0d-5313-45c3-927f-e32a41122dbf.png"
+import qr03 from "./03-4c2cd2fc-d512-4b4c-9605-34bbb96be554.png"
+import qr04 from "./04-c2812887-947d-4d82-b8fb-dc1b2e6cd7f6.png"
+import qr05 from "./05-315616bb-5870-4562-aeee-4196d62302c9.png"
+import qr06 from "./06-df6f98dd-81d2-465f-b658-cd2824eb3444.png"
+import qr07 from "./07-43486cc3-7cb7-450c-9321-b56bc38d6b81.png"
+import qr08 from "./08-18214c96-9fd5-4d60-a38b-41d4141b5deb.png"
+import qr09 from "./09-94336213-c603-42a8-a0e0-caeffc221372.png"
+import qr091 from "./091-d614f4d2-6d6a-49af-a412-c28c9dd5d365.png"
+import qr092 from "./092-43fada1c-c852-4545-a7f3-c177ddfc8402.png"
+import qr093 from "./093-4934161c-d09f-486c-a270-dcf8ec0d4605.png"
+import qr094 from "./094-e0fe9928-59f1-45e2-bb7e-f43c81ca5ca1.png"
+import qr095 from "./095-758e8eb9-e77f-4b6c-ace7-8aa7a2ed67b2.png"
+import qr096 from "./096-38878db0-20fa-4dfe-9bd6-41fe17c811d7.png"
+import qr097 from "./097-f5456400-30a5-4072-b871-121f4b3f86b0.png"
+import qr098 from "./098-2b2d8b55-448d-42d2-b311-d57e53d7596b.png"
+import qr099 from "./099-f8561173-75f3-4009-9d5c-8d8d7604f8e5.png"
+
+const ids = [
+  qrCode1,
+  qrCode2,
+  qrCode3,
+  qrCode4,
+  qrCode5,
+  qrCode6,
+  qrCode7,
+  qrCode8,
+  qrCode9,
+  qrCode10,
+  qrCode11,
+  qrCode12,
+  qrCode13,
+  qrCode14,
+  qrCode15,
+  qrCode16,
+  qrCode17,
+  qrCode18,
+]
+
 const qrData = {
-  [qrCode0]: {
-    title: 'Oo du hittade mig :O',
-    body: 'Så duktig.',
-    img: qrImage0
-  },
   [qrCode1]: {
-    title: 'QR = FUN',
-    body: 'Att en liten bild kan betyda så mycket.',
-    img: qrImage1
+    title: "Siffran",
+    body: "Nummer sju har alltid varit lyckans tal, men här kan det betyda mer än så. Är det en hylla? Ett fack? Början på en kombination?",
+    img: qr01,
   },
   [qrCode2]: {
-    title: 'QR QR på väggen där',
-    body: 'Vem vackraste koden är?',
-    img: qrImage2
+    title: "Kodens hemlighet",
+    body: "Siffrorna 3, 9 döljer en hemlighet (kanske en kombination för ett lås eller en kod?).",
+    img: qr02,
   },
   [qrCode3]: {
-    title: 'QR = LOVE',
-    body: 'När du skannar någon, kommer denne att finnas i ditt hjärta för alltid.',
-    img: qrImage3
+    title: "Platsens hemlighet",
+    body: "Du känner på pappret, det är fuktigt.. Skatten måste ligga på en fuktig plats!",
+    img: qr03,
   },
   [qrCode4]: {
-    title: 'QR = VISDOM',
-    body: 'Livet behöver inte vara perfekt så länge man har QR koder.',
-    img: qrImage4
-  }
+    title: "Ljusets blickar",
+    body: "Platsen måste vara något som både är mörkt och ljust. Var finner man skuggor som avslöjas?",
+    img: qr04,
+  },
+  [qrCode5]: {
+    title: "Dörrar",
+    body: "Sehär en ledtråd! Den lyder 'Platsen är alltid mörk så länge den inte är öppen'",
+    img: qr05,
+  },
+
+  [qrCode6]: {
+    title: "Lukten",
+    body: "Denna QR-kod luktar väldigt gott, hum vad kan det betyda?",
+    img: qr06,
+  },
+
+  [qrCode7]: {
+    title: "Smaken",
+    body: "Hmm, denna QR-kod smakade väldigt bra. Varför känns detta så bekant?",
+    img: qr07,
+  },
+
+  [qrCode8]: {
+    title: "Hemlighetens djup",
+    body: "Inte på ytan, utan längre in. Gräv djupare för att hitta vad som döljs längst bak.",
+    img: qr08,
+  },
+
+  [qrCode9]: {
+    title: "Bokstav",
+    body: "Konstigt, denna QR-kod visar endast bokstaven K",
+    img: qr09,
+  },
+
+  [qrCode10]: {
+    title: "Ljudets herre",
+    body: "Här dånar musiken men inget annat spår, det känns som om detta endast är en avledning!",
+    img: qr091,
+  },
+
+  [qrCode11]: {
+    title: "Taket",
+    body: "Här lyser endast stjärnorna kan inte vara något viktigt. (Du hör musik? Vart kommer detta ljudet ifrån?)",
+    img: qr092,
+  },
+
+  [qrCode12]: {
+    title: "Paniken",
+    body: "Här har någon varit förut! Flera måste vara på jakt efter skatten skynda!!",
+    img: qr093,
+  },
+
+  [qrCode13]: {
+    title: "Smyga",
+    body: "Bäst att inte visa för andra vart ledtrådarna är!",
+    img: qr094,
+  },
+
+  [qrCode14]: {
+    title: "Smulorna",
+    body: "Denna var smutsig! Inget mera att se här dock.",
+    img: qr095,
+  },
+
+  [qrCode15]: {
+    title: "Bokstäverna",
+    body: "Hum denna QR-kod var täckt av smulor och smuts, best jag för blickarna nedåt",
+    img: qr096,
+  },
+
+  [qrCode16]: {
+    title: "Usch",
+    body: "Bara en dåre skulle gömma skatten på ett sådant här ställe, bäst jag letar vidare.",
+    img: qr097,
+  },
+
+  [qrCode17]: {
+    title: "Hjulet",
+    body: "Oj vad det snurrar",
+    img: qr098,
+  },
+
+  [qrCode18]: {
+    title: "Trappan",
+    body: "Vilken bra höjd!",
+    img: qr099,
+  },
 }
 
 function useForceUpdate(): () => void {
@@ -60,42 +186,28 @@ function useForceUpdate(): () => void {
 }
 
 export function App() {
-  const location = useLocation()
   const history = useHistory()
   const query = useQuery()
   const forceUpdate = useForceUpdate()
 
-  const id = query.get('id') as QRCodes | null
+  const id = query.get("id") as QRCodes | null
   const foundQRCodes = getFoundQrCodes()
 
   useEffect(() => {
-    if (id !== null) {
+    if (id !== null && ids.includes(id)) {
       saveFoundQRCode(id)
     }
 
     if (isFirstFind()) {
       setFirstFind()
-      history.push('first-match')
+      history.push("first-match")
     } else {
       forceUpdate()
     }
   }, [id, history])
 
   const handleGohome = () => {
-    history.push('/')
-  }
-
-  const handlePlayAgain = () => {
-    resetGame()
-    const queryParams = new URLSearchParams(location.search)
-    if (queryParams.has('id')) {
-      queryParams.delete('id')
-      history.replace({
-        search: queryParams.toString()
-      })
-    }
-    history.go(0)
-    resetGame()
+    history.push("/")
   }
 
   return (
@@ -118,30 +230,31 @@ export function App() {
       <Switch>
         <Route exact path="/">
           <Container className="mt-4">
-            {foundQRCodes.length === 5 && (
-              <Alert variant={'success'}>
-                Grattis du har hittat alla qr koder {'<3'}.
+            {foundQRCodes.length >= 18 && (
+              <Alert variant={"success"}>
+                Vad kan alla ledtrådar betyda? Måste vara något på K som är
+                kallt, fuktigt och endast ljust när dörren är öppen!
               </Alert>
             )}
-
-            {foundQRCodes.length === 5 && (
-              <Button
-                variant="danger"
-                className="mb-4"
-                onClick={handlePlayAgain}
-              >
-                Play again!
-              </Button>
-            )}
-
-            <p>Här listas alla QR koder du redan hittat :D</p>
+            <p>
+              <b>
+                Du är en äventyrare som har fått nys om en gömd skatt. För att
+                hitta skatten behöver du följa en serie ledtrådar gömda i
+                QR-koder.
+              </b>
+            </p>
+            <p>Här samlas alla ledtrådar du redan hittat!</p>
 
             {foundQRCodes.map((QRCode) => {
               const data = qrData[QRCode]
 
               return (
-                <Card key={QRCode} className="mb-4" bg="dark" text="light">
-                  <Card.Img variant="top" src={data.img} />
+                <Card
+                  key={QRCode}
+                  className="mb-4"
+                  bg={QRCode === id ? "success" : "dark"}
+                  text="light"
+                >
                   <Card.Body>
                     <Card.Title>{data.title}</Card.Title>
                     <Card.Text>{data.body}</Card.Text>
@@ -158,20 +271,31 @@ export function App() {
             <p>
               Reglerna är enkla.
               <br />
-              Leta efter de gömda QR koder och skanna dom med din mobil
+              Leta efter de gömda QR koderna och skanna dom med din mobil
               kamera.
               <br />
               <br />
-              Du får ej avlägsna qr koden från sin plats
+              Du får ej avlägsna QR-koden från sin plats.
+              <br />
+              <br />
+              Berätta inte för någon vart skatten eller QR-koder befinner sig.
             </p>
+
+            <button onClick={resetGame}>Reset game :O</button>
           </Container>
         </Route>
         <Route path="/first-match">
           <Container className="mt-4">
-            <h1>Välkommen till QR spelet!</h1>
+            <h1>Skattjakten!</h1>
             <p>
-              I detta hem finns det gömda QR koder. Ditt mål är att hitta och
-              skanna alla. Vem vet det kanske finns ett pris?
+              Du är en äventyrare som har fått nys om en gömd skatt. För att
+              hitta skatten behöver du följa en serie ledtrådar gömda i
+              QR-koder.
+            </p>
+
+            <p>
+              För att skanna en qr kod använder du din mobilkamera eller en qr
+              kod app.
             </p>
 
             <Button variant="primary" onClick={handleGohome}>
--- my-app/src/lib/localstorage-handler.ts
+++ my-app/src/lib/localstorage-handler.ts
@@ -1,17 +1,43 @@
-const isFirstFindKeyValue = 'is-first-find'
+const isFirstFindKeyValue = "is-first-find"
 
-export const qrCode0 = '677d572f-1392-475a-bd70-491b530f6dfd'
-export const qrCode1 = 'a47a540d-3713-4270-8b00-a5101ee7be07'
-export const qrCode2 = 'cfccb9ab-adab-45f3-9b8c-652815e84fdb'
-export const qrCode3 = 'f1f97ade-f59a-4034-9d28-575d587a124d'
-export const qrCode4 = '62937f4e-e76d-4a99-8372-175efc548194'
+export const qrCode1 = "8ce5e78f-71c6-4827-9f52-0a4762c0e62c"
+export const qrCode2 = "c6c08b0d-5313-45c3-927f-e32a41122dbf"
+export const qrCode3 = "4c2cd2fc-d512-4b4c-9605-34bbb96be554"
+export const qrCode4 = "c2812887-947d-4d82-b8fb-dc1b2e6cd7f6"
+export const qrCode5 = "315616bb-5870-4562-aeee-4196d62302c9"
+export const qrCode6 = "df6f98dd-81d2-465f-b658-cd2824eb3444"
+export const qrCode7 = "43486cc3-7cb7-450c-9321-b56bc38d6b81"
+export const qrCode8 = "18214c96-9fd5-4d60-a38b-41d4141b5deb"
+export const qrCode9 = "94336213-c603-42a8-a0e0-caeffc221372"
+export const qrCode10 = "d614f4d2-6d6a-49af-a412-c28c9dd5d365"
+export const qrCode11 = "43fada1c-c852-4545-a7f3-c177ddfc8402"
+export const qrCode12 = "4934161c-d09f-486c-a270-dcf8ec0d4605"
+export const qrCode13 = "e0fe9928-59f1-45e2-bb7e-f43c81ca5ca1"
+export const qrCode14 = "758e8eb9-e77f-4b6c-ace7-8aa7a2ed67b2"
+export const qrCode15 = "38878db0-20fa-4dfe-9bd6-41fe17c811d7"
+export const qrCode16 = "f5456400-30a5-4072-b871-121f4b3f86b0"
+export const qrCode17 = "2b2d8b55-448d-42d2-b311-d57e53d7596b"
+export const qrCode18 = "f8561173-75f3-4009-9d5c-8d8d7604f8e5"
 
 export type QRCodes =
-  | typeof qrCode0
   | typeof qrCode1
   | typeof qrCode2
   | typeof qrCode3
   | typeof qrCode4
+  | typeof qrCode5
+  | typeof qrCode6
+  | typeof qrCode7
+  | typeof qrCode8
+  | typeof qrCode9
+  | typeof qrCode10
+  | typeof qrCode11
+  | typeof qrCode12
+  | typeof qrCode13
+  | typeof qrCode14
+  | typeof qrCode15
+  | typeof qrCode16
+  | typeof qrCode17
+  | typeof qrCode18
 
 export function isFirstFind(): boolean {
   const isFirst = localStorage.getItem(isFirstFindKeyValue)
@@ -28,19 +54,51 @@ export function saveFoundQRCode(code: QRCodes) {
 }
 
 export function hasFoundQRCode(code: QRCodes) {
-  const item = localStorage.getItem(code) ?? ''
+  const item = localStorage.getItem(code) ?? ""
 
-  return [qrCode0, qrCode1, qrCode2, qrCode3, qrCode4].includes(item)
+  return [
+    qrCode1,
+    qrCode2,
+    qrCode3,
+    qrCode4,
+    qrCode5,
+    qrCode6,
+    qrCode7,
+    qrCode8,
+    qrCode9,
+    qrCode10,
+    qrCode11,
+    qrCode12,
+    qrCode13,
+    qrCode14,
+    qrCode15,
+    qrCode16,
+    qrCode17,
+    qrCode18,
+  ].includes(item)
 }
 
 export function getFoundQrCodes(): QRCodes[] {
   const found: QRCodes[] = []
 
-  if (hasFoundQRCode(qrCode0)) found.push(qrCode0)
   if (hasFoundQRCode(qrCode1)) found.push(qrCode1)
   if (hasFoundQRCode(qrCode2)) found.push(qrCode2)
   if (hasFoundQRCode(qrCode3)) found.push(qrCode3)
   if (hasFoundQRCode(qrCode4)) found.push(qrCode4)
+  if (hasFoundQRCode(qrCode5)) found.push(qrCode5)
+  if (hasFoundQRCode(qrCode6)) found.push(qrCode6)
+  if (hasFoundQRCode(qrCode7)) found.push(qrCode7)
+  if (hasFoundQRCode(qrCode8)) found.push(qrCode8)
+  if (hasFoundQRCode(qrCode9)) found.push(qrCode9)
+  if (hasFoundQRCode(qrCode10)) found.push(qrCode10)
+  if (hasFoundQRCode(qrCode11)) found.push(qrCode11)
+  if (hasFoundQRCode(qrCode12)) found.push(qrCode12)
+  if (hasFoundQRCode(qrCode13)) found.push(qrCode13)
+  if (hasFoundQRCode(qrCode14)) found.push(qrCode14)
+  if (hasFoundQRCode(qrCode15)) found.push(qrCode15)
+  if (hasFoundQRCode(qrCode16)) found.push(qrCode16)
+  if (hasFoundQRCode(qrCode17)) found.push(qrCode17)
+  if (hasFoundQRCode(qrCode18)) found.push(qrCode18)
 
   return found
 }
